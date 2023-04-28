5421d3aaadb345547702a18be43b48321540bce9
NimajnebEC <NimajnebEC@users.noreply.github.com>
2023-04-28 18:12:21 +0100

    Added empty library view

--- costumizer-ui/src/frontend/routes/Library/index.tsx
+++ costumizer-ui/src/frontend/routes/Library/index.tsx
@@ -3,6 +3,7 @@ import { For, Show, createResource } from "solid-js";
 import { faCirclePlus } from "@fortawesome/free-solid-svg-icons";
 import styles from "./styles.module.scss";
 import Card from "../../global/Card";
+import { A } from "@solidjs/router";
 import Fa from "solid-fa";
 
 export default function Library() {
@@ -10,34 +11,45 @@ export default function Library() {
 
 	return (
 		<>
-			<h1>Costumes</h1>
-			<div class={styles.list}>
+			<Show
+				when={!costumes.loading && costumes()?.data}
+				fallback={
+					<For each={new Array(8)}>{() => <div class={styles.loading_entry} />}</For>
+				}
+			>
 				<Show
-					when={!costumes.loading && costumes()?.data}
+					when={costumes()!.data!.length > 0}
 					fallback={
-						<For each={new Array(8)}>{() => <div class={styles.loading_entry} />}</For>
+						<div class={styles.empty}>
+							<span class={styles.icon}>✨</span>
+							<p>You do not have any costumes yet</p>
+							<A href="/new">Create New Costume</A>
+						</div>
 					}
 				>
-					<For each={costumes()!.data}>
-						{(costume) => (
-							<Card
-								class={styles.entry}
-								title={costume.name}
-								href={"/costume/" + costume.name}
-							>
-								<div
-									class={styles.preview}
-									style={{ "background-image": `url(${costume.preview})` }}
-								/>
-							</Card>
-						)}
-					</For>
-					<Card class={styles.new} href="/new">
-						<Fa icon={faCirclePlus} class={styles.plus} />
-						New Costume
-					</Card>
+					<h1>Costumes</h1>
+					<div class={styles.list}>
+						<For each={costumes()!.data}>
+							{(costume) => (
+								<Card
+									class={styles.entry}
+									title={costume.name}
+									href={"/costume/" + costume.name}
+								>
+									<div
+										class={styles.preview}
+										style={{ "background-image": `url(${costume.preview})` }}
+									/>
+								</Card>
+							)}
+						</For>
+						<Card class={styles.new} href="/new">
+							<Fa icon={faCirclePlus} class={styles.icon} />
+							New Costume
+						</Card>
+					</div>
 				</Show>
-			</div>
+			</Show>
 		</>
 	);
 }
