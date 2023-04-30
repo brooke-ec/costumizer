b886c037f338b9557f6b6d0c7a7521a792e434b9
NimajnebEC <NimajnebEC@users.noreply.github.com>
2023-04-30 19:04:27 +0100

    Setup plugin and paperweight

--- costumizer-plugin/src/main/java/net/nimajnebec/costumizer/Costumizer.java
+++ costumizer-plugin/src/main/java/net/nimajnebec/costumizer/Costumizer.java
@@ -1,13 +1,15 @@
 package net.nimajnebec.costumizer;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.slf4j.Logger;
 
 public final class Costumizer extends JavaPlugin {
+    Logger logger = this.getSLF4JLogger();
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        logger.info("{} {} Loaded!", this.getName(), this.getPluginMeta().getVersion());
+        
     }
 
     @Override
