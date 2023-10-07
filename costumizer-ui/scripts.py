2e9d4861ecb08731b4a8a67ef392c9d2e8363287
NimajnebEC <nimajnebec@users.noreply.github.com>
2023-10-07 01:54:50 +0100

    feat: upgrade build to occour in dockerfile

--- costumizer-ui/scripts.py
+++ costumizer-ui/scripts.py
@@ -1,7 +1,7 @@
-import subprocess
+import os
 import shutil
 import signal
-import os
+import subprocess
 
 package_manager: str | None = (
     shutil.which("pnpm") if shutil.which("pnpm") else shutil.which("npm")
@@ -39,55 +39,3 @@ def dev():
     )
     vite.wait()
     os.kill(flask.pid, signal.CTRL_C_EVENT)
-
-
-def build():
-    poetry_path: str | None = shutil.which("poetry")
-    if poetry_path is None:
-        raise FileNotFoundError("Could not find poetry")
-
-    docker_path: str | None = shutil.which("docker")
-    if docker_path is None:
-        raise FileNotFoundError("Could not find docker")
-
-    # Vite Build
-    p = subprocess.run([package_manager, "exec", "vite", "build"], shell=True)
-    if p.returncode != 0:
-        return
-
-    # Get Poetry Requirements
-    p = subprocess.run(
-        [
-            poetry_path,
-            "export",
-            "-f",
-            "requirements.txt",
-            "-o",
-            "./build/requirements.txt",
-            "--without-hashes",
-        ],
-        shell=True,
-    )
-    if p.returncode != 0:
-        return
-
-    shutil.copytree("./src/backend/", "./build/app", dirs_exist_ok=True)
-    shutil.copy("./src/Dockerfile", "./build/")
-
-    p = subprocess.run(
-        [
-            docker_path,
-            "build",
-            "--pull",
-            "--rm",
-            "-t",
-            "costumizer-ui:latest",
-            ".",
-        ],
-        cwd="./build",
-        shell=True,
-    )
-    if p.returncode != 0:
-        return
-
-    print("costumizer-ui built!")
--- costumizer-ui/vite.config.ts
+++ costumizer-ui/vite.config.ts
@@ -17,6 +17,6 @@ export default defineConfig({
 	},
 	build: {
 		target: "esnext",
-		outDir: "../../build/static",
+		outDir: "../../dist",
 	},
 });
